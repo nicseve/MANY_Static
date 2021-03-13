f5418c30e7844812184866d73e482a310eed46fa
March <march@Marchs-MacBook-Air.local>
2021-03-13 14:34:44 -0800

    Added array of text elements for months

--- src/pages/whatDurationMonths.js
+++ src/pages/whatDurationMonths.js
@@ -2,73 +2,46 @@ import React from 'react';
 import {MainContainer, IphoneFrame, IphoneScreen} from "../components";
 
 
-// TODO: Map text elements to array of ints
-
 export default class WhatDurationMonths extends React.Component {
     render() {
+        const ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20];
+        const months = ints.map(int => <text
+            style={{
+                color: 'white',
+                fontWeight: 'regular',
+                fontSize: '80px'
+            }}>{int}
+        </text>)
+
         return (
             <MainContainer>
                 <IphoneFrame/>
                 {/*Override iPhone background screen color*/}
                 <IphoneScreen style={{backgroundColor: '#161616'}}>
-                    <div style={{zIndex:20,flexDirection:'column',display:'flex',position: 'absolute', top: '25%', left: '41%', maxHeight: 470, overflow: "auto"}}>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>12
-                        </text>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>11
-                        </text>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>10
-                        </text>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>9
-                        </text>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>8
-                        </text>
-                        <text
-                            style={{
-                                color: 'white',
-                                fontWeight: 'regular',
-                                fontSize: '80px'
-                            }}>7
-                        </text>    <text
-                        style={{
-                            color: 'white',
-                            fontWeight: 'regular',
-                            fontSize: '80px'
-                        }}>6
+                    <text style={{
+                        position: 'absolute',
+                        color: 'white',
+                        marginLeft: 110,
+                        marginTop: 100,
+                        // backgroundColor: 'blue',
+                        fontWeight: 'regular',
+                        fontSize: '20px'
+                    }}>months
                     </text>
+                    <div style={{
+                        // backgroundColor:'blue',
+                        zIndex: 1,
+                        flexDirection: 'column',
+                        display: 'flex',
+                        position: 'absolute',
+                        marginTop: 3,
+                        marginLeft: 12,
+                        height: 536,
+                        overflow: "auto"
+                    }}>
+                        {months}
                     </div>
-                        <text style={{
-                            position:'absolute',
-                            color: 'white',
-                            // backgroundColor: 'blue',
-                            fontWeight: 'regular',
-                            fontSize: '20px'
-                        }}>months
-                        </text>
+
                 </IphoneScreen>
             </MainContainer>
         )
