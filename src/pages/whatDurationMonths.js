106cd461404bc32f68f00ef9267993392ad6b960
nicseve <sevenick@gmail.com>
2021-03-14 08:55:51 -0700

    Added draggable scroll library for testing

--- src/pages/whatDurationMonths.js
+++ src/pages/whatDurationMonths.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {MainContainer, IphoneFrame, IphoneScreen} from "../components";
+import ScrollContainer from "react-indiana-drag-scroll";
+
 
 
 export default class WhatDurationMonths extends React.Component {
     render() {
         let months = [];
-        for (let i = 0; i <= 24; i++) {
+        for (let i = 1; i <= 24; i++) {
             months.push(
                 <text
                     style={{
                         color: 'white',
                         fontWeight: 'regular',
-                        fontSize: '80px'
+                        fontSize: '80px',
+                        paddingBottom: i === 24 ? 395 : 0,
+                        paddingTop: i === 1 ? 40 : 0
                     }}>{i}
                 </text>)
         }
@@ -28,18 +32,23 @@ export default class WhatDurationMonths extends React.Component {
                         fontSize: '20px'
                     }}>months
                     </text>
-                    <div style={{
-                        zIndex: 1,
-                        flexDirection: 'column',
-                        display: 'flex',
-                        position: 'absolute',
-                        marginTop: 3,
-                        marginLeft: 12,
-                        height: 536,
-                        overflow: "auto"
-                    }}>
-                        {months}
-                    </div>
+                    {/*<div style={{*/}
+                    {/*    zIndex: 1,*/}
+                    {/*    flexDirection: 'column',*/}
+                    {/*    display: 'flex',*/}
+                    {/*    position: 'absolute',*/}
+                    {/*    marginTop: 3,*/}
+                    {/*    marginLeft: 12,*/}
+                    {/*    height: 536,*/}
+                    {/*    overflow: "auto"*/}
+                    {/*}}>*/}
+                    {/*    {months}*/}
+
+                        <ScrollContainer style={{flexDirection:'column',display:'flex',marginLeft:12,marginTop:3,height:536,width:100,zIndex:20,position:'relative'}}
+                                         horizontal={false}
+                        >
+                            {months}
+                        </ScrollContainer>
                 </IphoneScreen>
             </MainContainer>
         )
