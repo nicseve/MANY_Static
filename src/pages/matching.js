74e13cc7daa7cb63c9ce29b6978fc17e14815355
nicseve <sevenick@gmail.com>
2021-03-20 14:57:54 -0700

    Added bottom users array

--- src/pages/matching.js
+++ src/pages/matching.js
@@ -205,7 +205,104 @@ const mainUsers = [
     // },
 ];
 
-const createMainUsers = (usersDict) => {
+const secondaryUsers = [
+    // {
+    //     move: 90,
+    //     z: 0,
+    //     month: 24,
+    //     id: 12,
+    //     location: require("../assets/continents/0075_con.png"),
+    //     glyph: require("../assets/glyphs/0075_C.png").default,
+    //     logo: require("../assets/logos/0075_WFC_BW.png")
+    // },
+    {
+        z: 0,
+        month: 12,
+        id: 13,
+        location: require("../assets/continents/0077_con.png"),
+        glyph: require("../assets/glyphs/0077_B.png").default,
+        logo: require("../assets/logos/0077_Jerash_BW.png")
+    },
+    // {
+    //     z: 0,
+    //     month: 12,
+    //     id: 14,
+    //     location: require("../assets/continents/0078_con.png"),
+    //     glyph: require("../assets/glyphs/0078_B.png").default,
+    //     logo: require("../assets/logos/0078_FAO_BW.png")
+    // },
+    // {
+    //     z: 1,
+    //     month: 36,
+    //     id: 15,
+    //     location: require("../assets/continents/0079_con.png"),
+    //     glyph: require("../assets/glyphs/0079_A.png").default,
+    //     logo: require("../assets/logos/0079_DamascusU_BW.png")
+    // },
+    // {
+    //     z: 1,
+    //     month: 24,
+    //     id: 16,
+    //     location: require("../assets/continents/0080_con.png"),
+    //     glyph: require("../assets/glyphs/0080_A.png").default,
+    //     logo: require("../assets/logos/0080_ECA_BW.png")
+    // },
+    {
+        z: 0,
+        month: 24,
+        id: 17,
+        location: require("../assets/continents/0081_con.png"),
+        glyph: require("../assets/glyphs/0081_B.png").default,
+        logo: require("../assets/logos/0081_WWBG_BW.png")
+    },
+    // {
+    //     z: 1,
+    //     month: 18,
+    //     id: 18,
+    //     location: require("../assets/continents/0085_con.png"),
+    //     glyph: require("../assets/glyphs/0085_A.png").default,
+    //     logo: require("../assets/logos/0085_BHRRC_BW.png"),
+    //     logo1: require("../assets/logos/0085_JCCA_BW.png")
+    // },
+    {
+        z: 0,
+        month: 18,
+        id: 19,
+        location: require("../assets/continents/0086_con.png"),
+        glyph: require("../assets/glyphs/0086_B.png").default,
+        logo: require("../assets/logos/0086_BIRZEIT_BW.png"),
+        logo1: require("../assets/logos/0086_IYF_BW.png")
+    },
+    // {
+    //     z: 0,
+    //     month: 6,
+    //     id: 20,
+    //     location: require("../assets/continents/0088_con.png"),
+    //     glyph: require("../assets/glyphs/0088_B.png").default,
+    //     logo: require("../assets/logos/0088_ESL_BW.png"),
+    //     logo1: require("../assets/logos/0088_icee_BW.png")
+    // },
+    {
+        z: 1,
+        month: 6,
+        id: 21,
+        location: require("../assets/continents/0089_con.png"),
+        glyph: require("../assets/glyphs/0089_A.png").default,
+        logo: require("../assets/logos/0089_DOCTORSACADEMY_BW.png")
+    },
+    {
+        z: 1,
+        month: 6,
+        id: 22,
+        location: require("../assets/continents/0091_con.png"),
+        glyph: require("../assets/glyphs/0091_A.png").default,
+        logo: require("../assets/logos/0091_CS_BW.png"),
+        logo1: require("../assets/logos/0091_LMA_BW.png")
+    },
+];
+
+
+const createUsers = (usersDict) => {
     let users = []
     usersDict.forEach((user => {
         users.push(
@@ -225,7 +322,8 @@ useDragToScroll({ref: snapList, disable: false});
 const snapList1 = useRef(null);
 useDragToScroll({ref: snapList1, disable: false});
 
-let users = createMainUsers(mainUsers);
+let usersTop = createUsers(mainUsers);
+let usersBottom = createUsers(mainUsers).reverse();
 
 return (
     <MainContainer>
@@ -234,13 +332,13 @@ return (
             <div style={{zIndex:4,position:'fixed',marginTop:100}}>
                 <SnapList ref={snapList} direction="horizontal"
                           width='308px' height='250px'>
-                    {users}
+                    {usersTop}
                 </SnapList>
             </div>
             <div style={{zIndex:3,position:'fixed',marginTop:210}}>
             <SnapList ref={snapList1} direction="horizontal"
                       width='308px' height='250px'>
-                {users}
+                {usersBottom}
             </SnapList>
             </div>
         </IphoneScreen>
