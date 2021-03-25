82ce5243551bc84ffdb70ffc83e238c06c18e733
nicseve <sevenick@gmail.com>
2021-03-24 19:38:54 -0700

    Refined duration page

--- src/pages/whatDuration.js
+++ src/pages/whatDuration.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import {MainContainer, IphoneFrame, TextHeaderCush, IphoneScreen, TextBaseGothic} from "../components";
+import {
+    MainContainer,
+    IphoneFrame,
+    TextHeaderCush,
+    IphoneScreen,
+    TextBaseGothic,
+    NavigationButton
+} from "../components";
 
 function WhatDuration() {
+    const monthsIntElement = <TextBaseGothic fontSize='80px'> 12 </TextBaseGothic>
+
     return (
         <MainContainer>
             <IphoneFrame/>
@@ -9,14 +18,10 @@ function WhatDuration() {
                 <div style={{margin: '15px'}}>
                     <TextHeaderCush>What is the duration?</TextHeaderCush>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'row', position: 'relative', top: '10px', left: '20px'}}>
-                    <TextBaseGothic fontSize='80px'>
-                        12
-                    </TextBaseGothic>
-                    <div style={{marginTop: '55px', marginLeft: '10px'}}>
-                        <TextBaseGothic fontSize='20px'>
-                            months
-                        </TextBaseGothic>
+                <div style={{display: 'flex', flexDirection: 'row', position: 'relative', top: '10px', left: '10px'}}>
+                    <NavigationButton displayComponent={monthsIntElement} path={'/whatDurationMonths'} height={'80px'}/>
+                    <div style={{marginTop: '50px'}}>
+                        <TextBaseGothic fontSize='20px'> months </TextBaseGothic>
                     </div>
                 </div>
             </IphoneScreen>
